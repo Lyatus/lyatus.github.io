aadef6eb73eec1523e10fed0fda0350415246613
Lyatus (LuduZord) <lutopia@hotmail.fr>
2015-11-04 20:58:34 +0100

    Skills are in Curriculum

--- js/pages.js
+++ js/pages.js
@@ -8,23 +8,12 @@ var pages = {
 	'Curriculum':{
 		'title':'Curriculum',
 		'content':
-			"<h2>Presentation</h2><p>My name is Lucien Catonnet and I\'m a 21 year-old video game programming student at ENJMIN. Before that, I studied computer science for three years at Jules Verne\'s University of Picardy. On this website I\'m putting most of the projects I\'ve worked on and writing what my work was in it. Feel free to contact me if you want to know more.</p>"
+			"<h2>Presentation</h2><p>My name is Lucien Catonnet and I'm a 21 year-old video game programming student at ENJMIN. I'm currently looking for an internship as a programmer in a video game studio.</p>"
+			+"<h2>Skills</h2><p><b>Languages</b>: C/C++, C#, Java, PHP, JavaScript, GLSL<br/><b>Middleware</b>: Unreal Engine 4, Visual Studio, Unity, Github, Code::blocks<br/><b>Libraries</b>: OpenGL, Wwise<br/><b>Spoken languages</b>: French (mother tongue), English (fluent)</p>"
 			+"<h2>Studies</h2><p>ENJMIN: Master's Degree in video game programming (2014 to 2016)<br/>UPJV: Bachelor's Degree in Computer Science (2011 to 2014)</p>"
 			+"<h2>Interests</h2><p>I love spending my time watching TV shows and movies (mostly TV shows recently). What drives me the most is clearly programming though, and especially everything low-level.</p>",
 		'images':['img/photo.jpg']
 	},
-	'Skills':{
-		'title':'Skills',
-		'content':
-			'<h2>Languages</h2>'
-			+'<p>C/C++, C#, Java, PHP, JavaScript, GLSL</p>'
-			+'<h2>Middleware</h2>'
-			+'<p>Unreal Engine 4, Visual Studio, Unity, Github, Code::blocks</p>'
-			+'<h2>Libraries</h2>'
-			+'<p>OpenGL, Wwise</p>'
-			+'<h2>Spoken languages</h2>'
-			+'<p>French (mother tongue), English (fluent)</p>'
-	}
 	/*'ENJMIN':
 		'<h1>ENJMIN</h1>'
 		+'<p><img src="img/enjmin/logo.jpg">ENJMIN (Ecole Nationale du Jeu et des Médias Interactifs Numériques) is a public video game school located in Angoulême, France. You can find their website and a bundle containing games made by their students in the links below. I\'m currently a student there in my first year of a Master\'s Degree and I\'m very fond of the way the school allows us to work with different fields of the video game industry, in order to understand the interactions that need to take place in an actual game development studio.</p>'
