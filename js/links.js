081709414cb2c9809eff1165a3589bd664ce0c13
Lyatus (LuduZord) <lutopia@hotmail.fr>
2016-07-21 22:04:06 +0200

    Added link to CodinGame account

--- js/links.js
+++ js/links.js
@@ -4,5 +4,6 @@ var links = {
 	'Twitter':'https://twitter.com/LucienCatonnet',
 	'LinkedIn':'https://www.linkedin.com/in/luciencatonnet',
 	'GitHub':'https://github.com/Lyatus',
-	'itch.io':'http://lutopia.itch.io'
+	'itch.io':'http://lutopia.itch.io',
+	'CodinGame':'https://www.codingame.com/profile/44722dd1152542ad315851af4246e774099192',
 };
