0bb7dfac85134cf014003ef2f5feceb79f453fbd
Lyatus (LuduZord) <lutopia@hotmail.fr>
2016-07-21 22:25:54 +0200

    Added Lua to known languages

--- js/pages.js
+++ js/pages.js
@@ -12,7 +12,7 @@ var pages = {
 			'content':
 				"<h2>Presentation</h2><p>My name is Lucien Catonnet and I'm a 22 year-old video game programming student at ENJMIN. I'm currently looking for an internship as a programmer in a video game studio. Ultimately I want to become a game engine programmer.</p>"
 				+"<h2>Skills</h2><p>"
-				+"<b>Languages</b>: C/C++, x86, C#, Java, PHP, JavaScript, GLSL"
+				+"<b>Languages</b>: C/C++, Lua, x86, C#, Java, PHP, JavaScript, GLSL"
 				+"<br/><b>Software</b>: Unreal Engine 4, Visual Studio, Code::Blocks, Unity, Git, Perforce"
 				+"<br/><b>Libraries</b>: OpenGL, Wwise, FreeType"
 				+"<br/><b>Spoken languages</b>: French (mother tongue), English (fluent)"
