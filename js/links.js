916b3ee13d02bde5070dac71d52ecc7706102c12
Lucien Catonnet <lutopia@hotmail.fr>
2018-03-29 17:22:17 +0200

    Removed most irrelevent personal links

--- js/links.js
+++ js/links.js
@@ -1,9 +1,6 @@
 var links = {
-	'Facebook':'https://www.facebook.com/luciencatonnet',
-	'Rotonde':'dat://lutopia.hashbase.io/',
 	'Twitter':'https://twitter.com/LucienCatonnet',
 	'LinkedIn':'https://www.linkedin.com/in/luciencatonnet',
 	'GitHub':'https://github.com/Lyatus',
 	'itch.io':'http://lutopia.itch.io',
-	'CodinGame':'https://www.codingame.com/profile/44722dd1152542ad315851af4246e774099192',
 };
