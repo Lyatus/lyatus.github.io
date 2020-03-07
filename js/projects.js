4203380efab23d7d2c7e47afc65e33cfd79f90c8
Lucien Catonnet <lutopia@hotmail.fr>
2020-03-07 18:03:20 +0100

    Added short information for Raccoon

--- js/projects.js
+++ js/projects.js
@@ -21,6 +21,11 @@ const projects = {
 	},
 	raccoon: {
 		thumbnail: 'project/raccoon/thumbnail.png',
+		short: [
+			"Browser-based fantasy console",
+			"Hot-reload code/data mechanism",
+			"Source control integration to allow team development",
+		],
 		content:
 			"<h2>Presentation</h2><p>Raccoon is a web fantasy console. It's inspired by <a href=\"https://www.lexaloffle.com/pico-8.php\" target=\"_blank\">PICO-8</a> but aims at offering better tools (not made within the constraints of the fantasy console). This is why for this project I work with a UX designer to try and get people to make games the easiest way possible. Raccoon wants to bring together game creation with constraints and great tools.</p>"
 			+"<h2>Console features</h2><p>"
